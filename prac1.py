fd52af6926caf28a98ebe1618e997e662071850c
20233861_엄바딤 <127921754+MEWXY@users.noreply.github.com>
2023-04-10 23:35:31 +0900

    Delete prac1.py

--- prac1.py
+++
@@ -1,8 +0,0 @@
-hw_score = input("Homework:")
-att_score = input("Attendance:")
-mid_score = input("Mid-term exam:")
-fin_score = input("Final exam:")
-
-total_score = hw_score + att_score + mid_score + fin_score
-
-print("Total score:", total_score)
