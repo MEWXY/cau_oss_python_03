fcaf4826576c262ef0604e68059d217ef9b31e82
20233861_엄바딤 <127921754+MEWXY@users.noreply.github.com>
2023-04-10 15:33:01 +0900

    Create prac1.py
    1주차 점수합산 예제 작성

---
+++ prac1.py
@@ -0,0 +1,7 @@
+hw_score = input("Homework:")
+att_score = input("Attendance:")
+mid_score = input("Mid-term exam:")
+fin_score = input("Final exam:")
+
+total_score = hw_score + att_score + mid_score + fin_score
+Print("Total score:", total score)
