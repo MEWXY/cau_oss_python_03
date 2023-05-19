9f8522ee12dc5beaefdef2e62b71aed85d392bd1
MEWXY <313top2003top@gmail.com>
2023-05-19 23:47:02 +0900

    implemented, OSS Python Homework #4

--- figure_main.py
+++ figure_main.py
@@ -1,16 +1,14 @@
-import math
-class line:
+import figure 
 
-  __length = 0
-  def __init__(self, length):
-      self.__length = length 
-  def set_length(self, length):
-      self.__length = length 
-  def get_length(self):
-      return self.__lenth
-  def area_square(length):
-      return length * length
-  def area_circle(length):
-      return length * length * math.pi
-  def area_regular_triangle(length):
-      return length * length * math.sqrt(3)/4
+myline = figure.line(10)
+
+square = figure.area_square(myline.get_length())
+print(square)
+
+myline.set_length(20)
+regular_triangle = figure.area_regular_triangle(myline.get_length())
+print(regular_triangle)
+
+myline.set_length(30)
+circle = figure.area_circle(myline.get_length())
+print(circle)
